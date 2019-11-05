9525c474a4d5187d58457373089f9f51de7bc856
Lugges991 <29121982+Lugges991@users.noreply.github.com>
2019-11-05 17:50:08 +0100

    login/logout handled properly with MM notification; save, print and send json to a given server

--- node_helper.js
+++ node_helper.js
@@ -1,81 +1,95 @@
+/*
+ * node_helper.js
+ *
+ * This script is responsible for starting the python face/emotion recognition program.
+ * It is also supposed to handle messages from the python face/emotion recognition program and delegate them to the according modules.
+ * 
+ *
+ * @author: Lucas Mahler @Lugges991
+ *
+ */
 'use strict'
 const NodeHelper = require('node_helper');
-const {PythonShell} = require("python-shell");
+const {
+    PythonShell
+} = require("python-shell");
 const onExit = require('signal-exit')
 var pythonStarted = false;
 var pyshell;
 
 module.exports = NodeHelper.create({
-    python_start: function(){
+    python_start: function() {
         const self = this;
         const options = {
             mode: 'json',
-            args: []}
-        
+            args: []
+        }
+
         // starting the python script
         self.pyshell = new PythonShell('modules/' + this.name + '/src/main.py', null);
 
-    // check if a message of the python script is comming in
-    self.pyshell.on('message', function (message) {
-      // A status message has received and will log
-      if (message.hasOwnProperty('status')){
-        console.log("[" + self.name + "] " + message.status);
-      }
+        // check if a message of the python script is comming in
+        self.pyshell.on('message', function(message) {
+            message = JSON.parse(message);
+            // A status message has received and will log
+            if (message.hasOwnProperty('status')) {
+                console.log("[" + self.name + "] " + message.status);
+            }
 
-      // Somebody new are in front of the camera, send it back to the Magic Mirror Module
-      if (message.hasOwnProperty('login')){
-        console.log("[" + self.name + "] " + "Users " + message.login.names.join(' - ') + " logged in.");
-        self.sendSocketNotification('user', {
-          action: "login",
-          users: message.login.names
+            // Somebody new are in front of the camera, send it back to the Magic Mirror Module
+            if (message.hasOwnProperty('login')) {
+                console.log("[" + self.name + "] " + "Users " + message.login.names.join(' - ') + " logged in.");
+                self.sendSocketNotification('user', {
+                    action: "login",
+                    users: message.login.names
+                });
+            }
+
+            // Somebody left the camera, send it back to the Magic Mirror Module
+            if (message.hasOwnProperty('logout')) {
+                console.log("[" + self.name + "] " + "Users " + message.logout.names.join(' - ') + " logged out.");
+                self.sendSocketNotification('user', {
+                    action: "logout",
+                    users: message.logout.names
+                });
+            }
         });
-      }
 
-      // Somebody left the camera, send it back to the Magic Mirror Module
-      if (message.hasOwnProperty('logout')){
-        console.log("[" + self.name + "] " + "Users " + message.logout.names.join(' - ') + " logged out.");
-        self.sendSocketNotification('user', {
-          action: "logout",
-          users: message.logout.names
+        // Shutdown node helper
+        self.pyshell.end(function(err) {
+            if (err) throw err;
+            console.log("[" + self.name + "] " + 'finished running...');
         });
-      }
-    });
 
-    // Shutdown node helper
-    self.pyshell.end(function (err) {
-      if (err) throw err;
-      console.log("[" + self.name + "] " + 'finished running...');
-    });
+        onExit(function(code, signal) {
+            self.destroy();
+        })
+    },
 
-    onExit(function (code, signal) {
-      self.destroy();
-    })
-  },
+    python_stop: function() {
+        this.destroy();
+    },
 
-  python_stop: function() {
-    this.destroy();
-  },
+    destroy: function() {
+        console.log("[" + this.name + "] " + "Terminate python");
+        this.pyshell.childProcess.kill();
+    },
 
-  destroy: function() {
-    console.log("[" + this.name + "] " + "Terminate python");
-    this.pyshell.childProcess.kill();
-  },
-
-  socketNotificationReceived: function(notification, payload) {
-    // Configuration are received
-    if(notification === 'CONFIG') {
-      this.config = payload
-      // Set static output to 0, because we do not need any output for MMM
-      this.config['output'] = 0;
-      if(!pythonStarted) {
-        pythonStarted = true;
-        this.python_start();
+    socketNotificationReceived: function(notification, payload) {
+        // Configuration are received
+        if (notification === 'CONFIG') {
+            this.config = payload
+            // Set static output to 0, because we do not need any output for MMM
+            this.config['output'] = 0;
+            if (!pythonStarted) {
+                pythonStarted = true;
+                this.python_start();
+            };
         };
-    };
-  },
+    },
 
-  stop: function() {
-    pythonStarted = false;
-    this.python_stop();
-  }
+    stop: function() {
+        pythonStarted = false;
+        this.python_stop();
+    }
 });
--- src/main.py
+++ src/main.py
@@ -1,15 +1,22 @@
+"""main.py
+this python program detects a face in an image provided by /dev/video0, recognizes it, if 
+an encoding is stored already, and finally detects the emotion of the recognized face.
+On recognition it sends a login/logout message to the agnoMirror module
+
+
+@author: Lucas Mahler @Lugges991
+"""
 import sys
 import cv2
 import json
 import time
 import numpy as np
 import face_recognition
+import time
 from keras.models import load_model
-
-
-def print_json(type, message):
-    print(json.dumps({type: message}))
-    sys.stdout.flush()
+from lib.helper import print_json, send_json, json_to_file
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 
 
@@ -17,9 +24,11 @@ video_capture = cv2.VideoCapture(0)
 
 emotion_dict= {0:'Angry' , 5:'Sad', 4:'Neutral', 1:'Disgusted', 6:'Surprised', 2:'in Fear', 3:'Happy'}
 
+print_json('status', 'loading emotion detection model')
 # Load the emotion detection model
 model = load_model('/home/pi/MM_test/modules/agnoMirror/src/data/emo_model.hdf5')
 
+print_json('status', 'loading facial encodings')
 # Load a sample picture and learn how to recognize it.
 lugges_image= face_recognition.load_image_file("/home/pi/MM_test/modules/agnoMirror/src/data/test.jpg")
 lugges_face_encoding = face_recognition.face_encodings(lugges_image)[0]
@@ -34,6 +43,8 @@ face_names = []
 prev_names = []
 process_this_frame = True
 
+cnt = 0
+
 while True:
     # Grab a single frame of video
     ret, frame = video_capture.read()
@@ -42,87 +53,95 @@ while True:
         # Resize frame of video to 1/4 size for faster face recognition processing
         small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
     except Exception as e:
-        print("small frame", frame.shape)
-        print(str(e))
+        print("i")
         sys.stdout.flush()
+
     # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
     rgb_small_frame = small_frame[:, :, ::-1]
     gray_frame = cv2.cvtColor(rgb_small_frame, cv2.COLOR_RGB2GRAY)
 
-    # Only process every other frame of video to save time
-    # Find all the faces and face encodings in the current frame of video
-    face_locations = face_recognition.face_locations(rgb_small_frame)
-    face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-    
-    emotions = []
-    for loc in face_locations:
-        face_img = gray_frame[loc[0]:loc[3], loc[2]:loc[1]]
-        try:
-            face_img = cv2.resize(face_img, (48,48))
-            emo = np.argmax(model.predict(np.reshape(face_img, [1, face_img.shape[0], face_img.shape[1],1])))
-            emotions.append(emo)
-        except Exception as e:
-            print("face img", frame.shape)
-            print(str(e))
-            sys.stdout.flush()
-    
-    face_names = []
-    for face_encoding in face_encodings:
-        # See if the face is a match for the known face(s)
-        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-        name = "Unknown"
+    if cnt % 30 == 0:
 
-        face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-        best_match_index = np.argmin(face_distances)
-        if matches[best_match_index]:
-            name = known_face_names[best_match_index]
+        # Only process every other frame of video to save time
+        # Find all the faces and face encodings in the current frame of video
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+        timestamp = time.time()
+        
+        emotions = []
+        # getting the emotions to the corresponding face images
+        for loc in face_locations:
+            face_img = gray_frame[loc[0]:loc[3], loc[2]:loc[1]]
+            try:
+                face_img = cv2.resize(face_img, (48,48))
+                emo = np.argmax(model.predict(np.reshape(face_img, [1, face_img.shape[0], face_img.shape[1],1])))
+                emotions.append(emo)
+            except Exception as e:
+                print("")
+                sys.stdout.flush()
+        
+        face_names = []
+        for face_encoding in face_encodings:
+            # See if the face is a match for the known face(s)
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+            name = "Unknown"
 
-        face_names.append(name)
+            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+            best_match_index = np.argmin(face_distances)
+            if matches[best_match_index]:
+                name = known_face_names[best_match_index]
+
+            # if the face is not unknown, add him to the names list
+            if name != "Unknown":
+                face_names.append(name)
 
 
 
-    # Display the results
-    for (top, right, bottom, left), name, em in zip(face_locations, face_names, emotions):
-        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-        top *= 4
-        right *= 4
-        bottom *= 4
-        left *= 4
+        # Display the results
+        for (top, right, bottom, left), name, em in zip(face_locations, face_names, emotions):
+            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
 
-        # Draw a box around the face
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+            # Draw a box around the face
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
-        # Draw a label with a name below the face
-        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-        font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name + " is " + str(emotion_dict[em]), (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+            # Draw a label with a name below the face
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name + " is " + str(emotion_dict[em]), (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
-    logins = []
-    logouts = []
+        logins = []
+        logouts = []
+        
+        # check if there are new logins/logouts
+        for n, e in zip(face_names, emotions): 
+            if n not in prev_names:
+                logins.append(n)
+                json_to_file(n,timestamp,str(emotion_dict[e]))
 
-    print(face_names)
 
-    for n in face_names: 
-        if (n in prev_names == False) and n is not None:
-            logins.append(n)
+        for n in prev_names:
+            if n not in face_names:
+                logouts.append(n)
 
-    for n in prev_names:
-        if (n in face_names == False) and n is not None:
-            logouts.append(n)
+        # send notification to the MagicMirror module
+        if len(logins) > 0:
+            print_json("login", {"names": logins})
 
-    if len(logins) > 0:
-        print_json("login", {"names": logins})
+        if len(logouts) > 0:
+            print_json("logout", {"names": logouts})
 
-    if len(logouts) > 0:
-        print_json("logout", {"names": logins})
+        prev_names = face_names
+    cnt += 1
 
-    prev_names = face_names
     # Display the resulting image
     cv2.imshow('Video', frame)
     # Hit 'q' on the keyboard to quit!
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-    # time.sleep(1)
 
 
 
