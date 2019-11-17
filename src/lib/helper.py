606f304510693ca19bf8c16f52c29f8dafef3d89
Lugges991 <29121982+Lugges991@users.noreply.github.com>
2019-11-17 15:48:46 +0100

    added docstrings, fixed typos

--- src/lib/helper.py
+++ src/lib/helper.py
@@ -12,19 +12,25 @@ import face_recognition
 from pathlib import Path
 
 
-# path = "./modules/agnoMirror/src/data/images"
-path = "./src/data/images/"
+path = "./modules/agnoMirror/src/data/images/"
+
+
+# DEBUG ONLY
+# path = "./src/data/images/"
 
 def print_json(type, message):
+    """sends a json in the form of a MagicMirror notificaiton to stdout"""
     print(json.dumps({type: message}))
     sys.stdout.flush()
 
 def send_json(face_name, timestamp, emotion):
+    """sends the agno core api json container to the agno core api"""
     data = json.dumps({'account_id':'1', 'face_id': face_name, 'timestamp': time.ctime(int(timestamp)), 'emotion': emotion})
     headers = {'Content-type': 'application/json'}
     return requests.post('http://agno-dev.eu-central-1.elasticbeanstalk.com/api/users', data=data, headers=headers)
 
 def json_to_file(face_name, timestamp, emotion):
+    """save the agno core api json container to a file"""
     data = json.dumps({'face_id': str(face_name), 'timestamp': str(timestamp), 'emotion': str(emotion)})
     with open('./modules/agnoMirror/src/data/core_data/data_{}_{}.json'.format(face_name, timestamp), 'w') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
@@ -51,6 +57,13 @@ def compute_encodings():
 
 
 def get_names_encodings():
+    """
+    computes the directory index of the "path", if it has changed, save it as a pickle and 
+    calculate the new name and facial encoding pairs and finally save them as pickle. 
+    If cdi.pickle exists, load the saved name and facial encoding pairs
+
+    returns a list of names and a list of their corrseponding facial encodings
+    """
     dir_index = compute_dir_index()
     if os.path.isfile(path + "cdi.pickle") and pickle.load(open(path + "cdi.pickle", "rb")) != dir_index:
         n, e = compute_encodings()
@@ -65,7 +78,8 @@ def get_names_encodings():
     return n, e
 
 def compute_dir_index():
-    """ Return a tuple containing:
+    """
+    Return a tuple containing:
     - list of files (relative to path)
     - lisf of subdirs (relative to path)
     - a dict: filepath => last 
